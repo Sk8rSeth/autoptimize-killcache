214be9e5a7c2a70f47f1faa70b1c750c8e04ab74
Sk8rSeth <sethhowell2197@gmail.com>
2017-09-28 14:23:05 -0700

    added filesize functionality, framework for time interval

--- autoptimize-killcache.php
+++ autoptimize-killcache.php
@@ -1,6 +1,38 @@
 <?php
 
 // v0.1
+// $metric - what type of interval to use
+//     - $metric = 'time' -> if you want the cache deleted on a time interval in hours (ie '24', '90')
+//     - $metric = 'filesize' (DEFAULT) -> if you want the cache to be deleted based on the filesize of the cache in MB (ie '256', '1024')
+//  $interval - the interval to be used with the chosen $metric. only integer or equivilant string.
 function autoptimizeKillCache(){
+    $metric = 'time';
+    $interval = '24';
 
+    if ($metric == 'time') {
+        if (class_exists('autoptimizeCache')) {
+            $statArr=autoptimizeCache::stats();
+            $cacheSize=round($statArr[1]/1024);
+
+            if ($cacheSize>$metric){
+               autoptimizeCache::clearall();
+               header("Refresh:0"); // Refresh the page so that autoptimize can create new cache files and it does break the page after clearall.
+            }
+        }
+
+    } elseif ($metric == 'filesize') {
+        if (class_exists('autoptimizeCache')) {
+            $statArr=autoptimizeCache::stats();
+            $cacheSize=round($statArr[1]/1024);
+
+            if ($cacheSize>$metric){
+               autoptimizeCache::clearall();
+               header("Refresh:0"); // Refresh the page so that autoptimize can create new cache files and it does break the page after clearall.
+            }
+        }
+
+    } else {
+        // if its some other random value
+
+    }
 }
